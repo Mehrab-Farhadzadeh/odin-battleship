7ba38234c0ddc8e113917942bcfcaf05a1102e1a
Mehrab Farhadzadeh <mehrab7fm@gmail.com>
2026-02-23 13:54:01 +0330

    chore: refactor Ship
    isolate tests and follow the Few and Thorough rules.testing isSunk also
    verifies the correctness of `hit()`

--- logic/Ship.js
+++ logic/Ship.js
@@ -1,16 +1,17 @@
 class Ship {
-   hits = 0;
-   constructor(length_) {
-      this.length = length_;
+   #length;
+   #hits = 0;
+   constructor(length_ = 0) {
+      this.#length = length_;
    }
 
    hit() {
       if (this.isSunk()) throw RangeError("Has already been sunk");
-      this.hits++;
+      this.#hits++;
    }
 
    isSunk() {
-      return this.hits === this.length;
+      return this.#hits === this.#length;
    }
 }
 
